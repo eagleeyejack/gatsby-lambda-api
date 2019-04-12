ced8f3ae705c2f5c1cdcb338e7147a6504f92702
Jack Evans <jack@bluegg.co.uk>
2019-04-12 16:09:49 +0100

    Query hitting custom twitter API

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Twitter API`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -25,6 +25,22 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-source-apiserver",
+      options: {
+        // Type prefix of entities from server
+        typePrefix: "internal__",
+
+        // The url, this should be the endpoint you are attempting to pull data from
+        url: `https://angry-bardeen-2845b6.netlify.com/.netlify/functions/server/twitter`,
+
+        method: "get",
+
+        // Name of the data to be downloaded.  Will show in graphQL or be saved to a file
+        // using this name. i.e. posts.json
+        name: `twitter`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,101 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import get from "lodash/get"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=jackdavidevans&count=2&include_rts=false
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class IndexPage extends React.Component {
+  render() {
+    function urlify(text) {
+      var urlRegex = /(https?:\/\/[^\s]+)/g
+      return text.replace(urlRegex, function(url) {
+        return '<a href="' + url + '">' + url + "</a>"
+      })
+    }
+
+    const tweet = get(
+      this,
+      "props.data.allInternalTwitter.edges[0].node.twitterFile"
+    )
+
+    return (
+      <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <div style={{ padding: "1rem 0" }}>
+          <a
+            href={tweet.url}
+            style={{ textDecoration: "none", color: "#222222" }}
+          >
+            <div
+              dangerouslySetInnerHTML={{
+                __html: urlify(tweet.tweets),
+              }}
+            />
+            <div href={tweet.url}>@{tweet.screenName}</div>
+          </a>
+
+          <div style={{ padding: "1rem 0" }}>
+            {tweet.all.map((node, index) => (
+              <div key={index} style={{ margin: "1rem 0" }}>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: urlify(node.text),
+                  }}
+                />
+                <p>@{node.user.screen_name}</p>
+              </div>
+            ))}
+          </div>
+
+          <div style={{ padding: "1rem 0" }}>
+            <h3>The API LINK</h3>
+            <a href="https://angry-bardeen-2845b6.netlify.com/.netlify/functions/server/twitter">
+              https://angry-bardeen-2845b6.netlify.com/.netlify/functions/server/twitter
+            </a>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    internalTwitter {
+      twitterFile {
+        all {
+          text
+        }
+      }
+    }
+    allInternalTwitter {
+      edges {
+        node {
+          twitterFile {
+            date
+            screenName
+            name
+            tweets
+            tweetId
+            url
+            all {
+              text
+              user {
+                name
+                screen_name
+              }
+              entities {
+                urls {
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
